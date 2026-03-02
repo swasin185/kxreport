06bdebe32a6b8ce5fbab0480841932cdb78e2c6d
swasin185 <swasin185@gmail.com>
2026-03-02 09:56:52 +0700

    /usr/bin/free -h

--- script/mem.sh
+++ script/mem.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-ps -o pid,user,rss,command -e | awk '
+echo "--------------------------- $(date +%F[%T]) -------------------------------"
+/usr/bin/free -h
+echo
+/usr/bin/ps -o pid,user,rss,command -e | awk '
   BEGIN { OFS="\t" }
   # Print the header
   NR==1 { print "PID", "USER", "RAM_MB", "COMMAND" }
@@ -11,7 +14,7 @@ ps -o pid,user,rss,command -e | awk '
   
 echo
 echo -e "JAVA\tUSER\tRAM_MB\tSURV\tNURS\tLONG\tJVM\tGC\tCLEAN"
-echo "-----------------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 for pid in $(pgrep -u tomcat); do
     # 1. Get System Info (User and RSS)
     SYS_INFO=$(ps -p $pid -o user,rss --no-headers)
@@ -20,15 +23,15 @@ for pid in $(pgrep -u tomcat); do
 
     # 2. Get JVM Info
     # We use 'tail -n 1' to skip the jstat header and handle the awk math
-    JVM_INFO=$(sudo jstat -gc $pid | awk 'NR>1 {printf "%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%d", $1/1024, $2/1024, $6/1024, $8/1024, $10/1024, $15}')
+    JVM_INFO=$(sudo /usr/bin/jstat -gc $pid | awk 'NR>1 {printf "%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%d", $1/1024, $2/1024, $6/1024, $8/1024, $10/1024, $15}')
 
     # 3. Print combined row
     echo -e "$pid\t$USER\t${RSS}\t$JVM_INFO"
 done
-echo "-----------------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------"
 echo 
 echo -e "Database Connections"
-sudo mysql -e \
+sudo /usr/bin/mysql -e \
     "SELECT user, db, count(*) as count, round(avg(time), 2) as avg_time,\
     round(avg(MEMORY_USED) / 1024, 2) as 'avg_MB', round(sum(MEMORY_USED) / 1024, 2) as 'total_MB' \
     FROM information_schema.processlist \
